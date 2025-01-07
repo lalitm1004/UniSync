a875956e13272b99ca88322e9c5ad85a39fca162
lalitm1004 <lalitm1004@gmail.com>
2025-01-07 17:06:04 +0530

    feat(scraper): add functionality to scrape html

---
+++ src/scraper.py
@@ -0,0 +1,53 @@
+import os
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+
+from dotenv import load_dotenv
+
+class Scraper:
+    def __init__(self, headless: bool = True) -> None:
+        
+        options = Options()
+        if headless:
+            options.add_argument("--headless=new")
+        
+        self.driver = webdriver.Chrome(options=options)
+        self.wait = WebDriverWait(self.driver, 15)
+
+    def scrape_weekly_schedule_html(self) -> str:
+        self.__login_erp()
+        return self.__grab_weekly_schedule_html()
+
+    def __login_erp(self) -> None:
+        load_dotenv()
+        LOGIN_URL = "https://prodweb.snu.in/psp/CSPROD/EMPLOYEE/HRMS/?cmd=login"
+
+        self.driver.get(LOGIN_URL)
+        
+        netid_input = self.driver.find_element(By.ID, "userid")
+        netid_input.clear()
+        netid_input.send_keys(os.getenv("SNU_NETID"))
+        
+        password_input = self.driver.find_element(By.ID, "pwd")
+        password_input.clear()
+        password_input.send_keys(os.getenv("SNU_PASSWORD"))
+        print(os.getenv("SNU_PASSWORD"))
+
+        submit_button = self.driver.find_element(By.CLASS_NAME, "psloginbutton")
+        submit_button.click()
+
+    def __grab_weekly_schedule_html(self) -> str:
+        WEEKLY_SCHEDULE_URL = "https://prodweb.snu.in/psp/CSPROD_1/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_SCHD_W"
+        
+        self.driver.get(WEEKLY_SCHEDULE_URL)
+        self.driver.switch_to.frame(self.driver.find_element(By.ID, "ptifrmtgtframe"))
+
+        schedule_html = self.driver.find_element(By.ID, "WEEKLY_SCHED_HTMLAREA")
+        return schedule_html.get_attribute("outerHTML")
+
+if __name__ == "__main__":
+    scraper = Scraper(headless=False)
+    print(scraper.scrape_weekly_schedule_html())
